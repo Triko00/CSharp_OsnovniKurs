1f3aa55825f2646305490a479e714d309022c5eb
Veljko Trajkovic <veljkotrajkovic007@gmail.com>
2020-10-26 13:34:28 +0100

    Bolja organizacija za Solution

---
+++ Cas4_Zadatak0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cas4_Zadatak_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
